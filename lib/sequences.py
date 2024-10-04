179391f08087505dcf0ac1a4dc48041f8dae9c89
TrungCorp <trungle9995@gmail.com>
2024-10-04 02:58:59 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    for k in range(length):
+        if k == 0:
+            fib_list.append(0)
+        elif k== 1:
+            fib_list.append(1)
+        else:
+            new_num = fib_list[-2] +fib_list[-1]
+            fib_list.append(new_num)
+    print(fib_list)
+
